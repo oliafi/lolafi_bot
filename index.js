29a5f60a216885334bd775585c4306467584f9ba
Olga Skobeleva <olga@cloudflare.com>
2018-07-29 10:21:37 +0100

    revert to default

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.post('/new-message', function(req, res) {
 
   //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
 
-  if (!message || message.text.toLowerCase().indexOf('Hello') < 0) {
+  if (!message || message.text.toLowerCase().indexOf('marco') < 0) {
     // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
     return res.end()
   }
@@ -26,10 +26,10 @@ app.post('/new-message', function(req, res) {
   // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
   axios
     .post(
-      'https://api.telegram.org/bot683669998:AAHQIoWTpmKRR7eVixZdqrbmnORHbiB__BE/sendMessage',
+      'https://api.telegram.org/bot646843854:AAE5N-7_QqU3I5a2bkMkH5HiKFwVVH8ftfM/sendMessage',
       {
         chat_id: message.chat.id,
-        text: 'Hello to you too!'
+        text: 'Polo!!'
       }
     )
     .then(response => {

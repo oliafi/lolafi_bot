b1951460023dea77acb261743bd1080facf5dc63
Olga Skobeleva <olga@cloudflare.com>
2018-07-29 11:08:02 +0100

    2nd if

--- index.js
+++ index.js
@@ -42,6 +42,33 @@ app.post('/new-message', function(req, res) {
       console.log('Error :', err)
       res.end('Error :' + err)
     })
+
+  if (!message || message.text.toLowerCase().indexOf('Кто такой Ваня Никулин?') < 0) {
+    // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
+    return res.end()
+  }
+
+  // If we've gotten this far, it means that we have received a message containing the word "marco".
+  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
+  // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
+  axios
+    .post(
+      'https://api.telegram.org/bot646843854:AAE5N-7_QqU3I5a2bkMkH5HiKFwVVH8ftfM/sendMessage',
+      {
+        chat_id: message.chat.id,
+        text: 'Долбоеб 3000!'
+      }
+    )
+    .then(response => {
+      // We get here if the message was successfully posted
+      console.log('Message posted')
+      res.end('ok')
+    })
+    .catch(err => {
+      // ...and here if it was not
+      console.log('Error :', err)
+      res.end('Error :' + err)
+    })
 })
 
 // Finally, start our server

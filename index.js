c92578e8fa7386dde0ff855e1cfd17c9a7e9f2b7
Olga Skobeleva <olga@cloudflare.com>
2018-07-29 12:00:11 +0100

    try 3

--- index.js
+++ index.js
@@ -1,130 +1,171 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
-const axios = require('axios')
+var TelegramBot = require('node-telegram-bot-api'),
+    // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
+    telegram = new TelegramBot("646843854:AAE5N-7_QqU3I5a2bkMkH5HiKFwVVH8ftfM", { polling: true });
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-) // for parsing application/x-www-form-urlencoded
+var emoji = require('node-emoji').emoji;
 
-//This is the route the API will call
-app.post('/new-message', function(req, res) {
-  const { message } = req.body
+telegram.on("text", (message) => {
+ 
 
-  //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
-/*
-  if (message.text.toLowerCase().indexOf('300') < 0) {
-  // If we've gotten this far, it means that we have received a message containing the word "marco".
-  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
-  // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
-  axios
-    .post(
-      'https://api.telegram.org/bot646843854:AAE5N-7_QqU3I5a2bkMkH5HiKFwVVH8ftfM/sendMessage',
-      {
-        chat_id: message.chat.id,
-        text: 'otsosi u tractorista!!'
-      }
-    )
-    .then(response => {
-      // We get here if the message was successfully posted
-      console.log('Message posted')
-      res.end('ok')
-    })
-    .catch(err => {
-      // ...and here if it was not
-      console.log('Error :', err)
-      res.end('Error :' + err)
-    })
-  }
-})
-*/
+var Hi = "hi";
+if (message.text.toString().toLowerCase().indexOf(Hi) === 0) {
+telegram.sendMessage(message.chat.id,"Hi " + message.from.first_name);
+}
+var bye = "bye";
+if (message.text.toString().toLowerCase().indexOf(bye) === 0) {
+telegram.sendMessage(message.chat.id,"Papai.. See U again. Have a nice day");
+}
+var Hai = "hai";
+if (message.text.toString().toLowerCase().indexOf(Hai) === 0) {
+telegram.sendMessage(message.chat.id,"Caakkkk!!!");
+}
+var Sihat = "sihat?";
+if (message.text.toString().toLowerCase().includes(Sihat)) {
+telegram.sendMessage(message.chat.id,"Alhamdulillah");
+telegram.sendMessage(message.chat.id,"Ana di sini sihat-sihat aje");
+}
+var Hehe = "hehe";
+if (message.text.toString().toLowerCase().indexOf(Hehe) === 0) {
+telegram.sendMessage(message.chat.id,"Huhu");
+}
+var Sedap = "sedap";
+if (message.text.toString().toLowerCase().indexOf(Sedap) === 0) {
+telegram.sendMessage(message.chat.id,"X puase ke giler?");
+}
+var Book = "book";
+if (message.text.toString().toLowerCase().indexOf(Book) === 0) {
+telegram.sendMessage(message.chat.id,"");
+}
+var Merajuk = "merajuk";
+if (message.text.toString().toLowerCase().indexOf(Merajuk) === 0) {
+telegram.sendMessage(message.chat.id,"Alololololo.. ciannye diaa");
+}
+var Majok = "majok";
+if (message.text.toString().toLowerCase().indexOf(Majok) === 0) {
+telegram.sendMessage(message.chat.id,"Alololololo.. ciannye diaa");
+}
+var Alamat = "alamat";
+if (message.text.toString().toLowerCase().indexOf(Alamat) === 0) {
+telegram.sendLocation(message.chat.id, 3.234151, 101.689981);
+telegram.sendMessage(message.chat.id, "Sini lah tempat tu.");
+}
+var what = ['idiot' || 'bodoh' || 'sial'];
+if (message.text.toString().toLowerCase().includes(what[0,1,2])) {
+telegram.sendMessage(message.chat.id, "Kepala Banaaaa.");
+/*telegram.kickChatMember(message.chat.id,  message.from.id);*/
+}
 
-  if(message.text.toLowerCase().indexOf("/start")!== -1){
-      telegram.sendMessage(message.chat.id, "WARNING!\nThis Bot is being tested, please do not make any operation here!");
-      telegram.sendMessage(message.chat.id, "AVISO!\nEsse robô está sendo testado, por favor não realize nenhuma operação aqui!");
-      telegram.sendMessage(message.chat.id, "Oi, tudo bem? Pode me dizer seu CEP?");
-  }
-  else if(message.text.toLowerCase().indexOf("88050")!== -1){
-      telegram.sendMessage(message.chat.id, "Legal! Atendemos nessa região :)\nO que você gostaria de pedir?");
-  }
-  else if(message.text.toLowerCase().indexOf("pizza")!== -1){
-    telegram.sendMessage(message.chat.id, "Pode me dizer o sabor da pizza? ");
+var Raya = "selamat hari raya";
+if (message.text.toString().toLowerCase().includes(Raya)) {
+telegram.sendMessage(message.chat.id, "Selamat Hari Raya");
+telegram.sendMessage(message.chat.id, "Maaf Zahir dan Batin");
+}
+var Pagi = "selamat pagi";
+if (message.text.toString().toLowerCase().includes(Pagi)) {
+telegram.sendMessage(message.chat.id, "Selamat pagi jugak..");
+telegram.sendMessage(message.chat.id, "Have a nice day");
+}
+var Puasa = "puasa tak";
+if (message.text.toString().toLowerCase().includes(Puasa)) {
+telegram.sendMessage(message.chat.id, "Saya robot la.");
+telegram.sendMessage(message.chat.id, "Robot tak wajib puasa");
+}
+var Puasa = "puasa tak";
+if (message.text.toString().toLowerCase().includes(Puasa)) {
+telegram.sendMessage(message.chat.id, "Saya robot la.");
+telegram.sendMessage(message.chat.id, "Robot tak wajib puasa");
+}
+var Naktanya = "nak tanya";
+if (message.text.toString().toLowerCase().includes(Naktanya)) {
+telegram.sendMessage(message.chat.id, "Ye.. apa dia?");
+}
+var Sukatak = "suka tak";
+if (message.text.toString().toLowerCase().includes(Sukatak)) {
+telegram.sendMessage(message.chat.id, "Suka sangat sangat!!");
+}
+var Hmm = "hmm";
+if (message.text.toString().toLowerCase().includes(Hmm)) {
+telegram.sendMessage(message.chat.id, "Nape? merajuk ke mengeluh?");
+}
+var Okaytak = "ok tak";
+if (message.text.toString().toLowerCase().includes(Okaytak)) {
+telegram.sendMessage(message.chat.id, "okay jeee");
+}
+var Giler = "le giler";
+if (message.text.toString().toLowerCase().includes(Giler)) {
+telegram.sendMessage(message.chat.id, "ek eleh");
+telegram.sendMessage(message.chat.id, "kau le giler");
+}
+var Ajar = ("sape ajar", "siapa ajar");
+if (message.text.toString().toLowerCase().includes(Ajar)) {
+telegram.sendMessage(message.chat.id, "Papa le yang ajar");
+}
+var Spapa = "siapa papa";
+if (message.text.toString().toLowerCase().includes(Spapa)) {
+telegram.sendMessage(message.chat.id, "Papa saya ialah Tuan Aiman_Cheng");
+}
+var Syg = "sayang tak";
+if (message.text.toString().toLowerCase().includes(Syg)) {
+telegram.sendMessage(message.chat.id, "Sayang sangat taw!");
+}
+var Buat = "buat apa tu";
+if (message.text.toString().toLowerCase().includes(Buat)) {
+telegram.sendMessage(message.chat.id, "Melanguk jee");
+telegram.sendMessage(message.chat.id, "Takde buat pape pun");
+}
+var Cantik = "cantik tak";
+if (message.text.toString().toLowerCase().includes(Cantik)) {
+telegram.sendMessage(message.chat.id, "Cantik cangat cangat.");
+telegram.sendMessage(message.chat.id, "Tomeyyyy  " + emoji.heart);
+}
+var Handsome = "handsome";
+if (message.text.toString().toLowerCase().includes(Handsome)) {
+telegram.sendMessage(message.chat.id, "Weekkkkkk!");
+}
+var Hensem = "hensem";
+if (message.text.toString().toLowerCase().includes(Hensem)) {
+telegram.sendMessage(message.chat.id, "Weekkkkkk!");
+}
+
+telegram.onText(/Cak/ , (message) => {  
+ telegram.sendMessage(message.chat.id, "Weekkkkkk!!!"); 
+});
+
+telegram.onText(/\/mula/, (message) => {  
+telegram.sendMessage(message.chat.id, "Welcome", {
+"reply_markup": {
+    "keyboard": [["Sample text", "Second sample"],   ["Keyboard"], ["I'm robot"]]
     }
-  else if(message.text.toLowerCase().indexOf("calabresa")!== -1){
-    sabor=message.text.toLowerCase();
-    telegram.sendMessage(message.chat.id, "Qual o tamanho?");
-  }
-  else if(message.text.toLowerCase().indexOf("grande")!== -1){
-    tamanho=message.text.toLowerCase();
-    telegram.sendMessage(message.chat.id, "Refrigerante?");
-  }
-  else if (message.text.toLowerCase().indexOf("sim")!== -1){
-    if (acompanhamento==''){
-    telegram.sendMessage(message.chat.id, "Qual?");
-    }
-    else{
-      mandarPedido(tamanho, sabor, acompanhamento, preco, message.chat.id);
-    }
-  }
-  else if (message.text.toLowerCase().indexOf("não")!== -1 || message.text.toLowerCase().indexOf("nao")!== -1){
-    telegram.sendMessage(message.chat.id, "Desculpe :(\n O que você gostaria de pedir então?");    
-    preco=0;
-    sabor='';
-    tamanho='';
-    acompanhamento='';
-  }
-  else if (message.text.toLowerCase().indexOf("coca")!== -1){
-      acompanhamento='Coca-cola';
-      telegram.sendMessage(message.chat.id, "Entendi!\nTemos as seguintes pizzarias:\n1 - Pizzaria da Mama (R$2,00)\n2 - Don Guaianases (R$35,00) \n3 - Cadiolli (R$70,00)\n\nSelecione a que desejar!");    
-  }
-  else if (message.text.toLowerCase().indexOf("1")!== -1){
-      if (acompanhamento==''){
-        var mensagem="Confirma pra mim então:\n 1 - Pizza "+tamanho+" de "+sabor;
-        preco=100;
-        telegram.sendMessage(message.chat.id, mensagem);    
-      }
-      else{
-        preco=200;
-        telegram.sendMessage(message.chat.id, "Confirma pra mim então:\n1 - Pizza "+tamanho+" de "+sabor+"\n2 - "+acompanhamento);
-      }
-    telegram.sendMessage(message.chat.id, "É isso mesmo?!");
-  }
-  else if (message.text.toLowerCase().indexOf("2")!== -1){
-      if (acompanhamento=''){
-        preco=3000;
-        telegram.sendMessage(message.chat.id, "Confirma pra mim então:\n1 - Pizza "+tamanho+" de "+sabor);    
-      }
-      else{
-        preco=3500;
-        telegram.sendMessage(message.chat.id, "Confirma pra mim então:\n1 - Pizza "+tamanho+" de "+sabor+"\n2 - "+acompanhamento);
-      }
-    telegram.sendMessage(message.chat.id, "É isso mesmo?!");      
-  }
-  else if (message.text.toLowerCase().indexOf("3")!== -1){
-      if (acompanhamento=''){
-        preco=6500;
-        telegram.sendMessage(message.chat.id, "Confirma pra mim então:\n1 - Pizza "+tamanho+" de "+sabor);    
-      }
-      else{
-        preco=7000;
-        telegram.sendMessage(message.chat.id, "Confirma pra mim então:\n1 - Pizza "+tamanho+" de "+sabor+"\n2 - "+acompanhamento);
-      }
-    telegram.sendMessage(message.chat.id, "É isso mesmo?!");      
-  }
-  else if (message.text.toLowerCase().indexOf("/stop")!== -1){
-    new StopController()
-  }
-  else{
-    telegram.sendMessage(message.chat.id, "Desculpe, não entendi o que quis dizer :("); 
-  }
-  
-})
+});  
+});
 
+telegram.onText(/\/marykay/, (message) => {  
+telegram.sendMessage(message.chat.id, "Hai! Selamat Datang.. Sila Pilih..", {
+"reply_markup": {
+    "keyboard": [["Pengenalan", "Product"],   ["Pertanyaan"]]
+    }
+});  
+});
 
-// Finally, start our server
-app.listen(3000, function() {
-  console.log('Telegram app listening on port 3000!')
-})
+var Lapar = "lapar";
+if (message.text.toString().toLowerCase().includes(Lapar)) {
+telegram.sendMessage(message.chat.id, "Saya pun.");
+} 
+
+var Pengenalan = "pengenalan";
+if (message.text.toString().toLowerCase().indexOf(Pengenalan) === 0) {
+    telegram.sendMessage(message.chat.id, "Hai, Saya Dakcik Adda. Agent Mary Kay");
+    telegram.sendMessage(message.chat.id, "Mary Kay ni produk bla bla bla");
+}
+
+var Product="product";
+if (message.text.toString().toLowerCase().indexOf(Product) === 0) {
+telegram.sendMessage(message.chat.id, "Ini antara produk yang saya jual");
+telegram.sendPhoto(message.chat.id,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuSqXRgWHNX-lu51zJlQIPDKCigMCDjdS7sTRbLJ50zH1TLguU",{caption:"Ini Lipstik. Harga bla bla per item"});
+telegram.sendPhoto(message.chat.id,"https://gd.image-gmkt.com/100-AUTHENTIC-MARY-KAY-SKINVIGORATE-CLEANSING-BRUSH-TIMEWISE/li/412/233/851233412.g_400-w_g.jpg",{caption:"Ini set kosmetik. Harga bla bla"});
+}
+var Pertanyaan = "pertanyaan";
+if (message.text.toString().toLowerCase().indexOf(Pertanyaan) === 0) {
+telegram.sendMessage(message.chat.id,"Hai. Ada apa-apa yang boleh saya bantu?");
+}
+});
